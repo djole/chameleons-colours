905a8c8ccf34468ebc374efbd112559cead90931
djole <dorde.grbic@gmail.com>
2013-05-03 18:38:07 +0200

    Reduce word size. Less processors. Add progress prints

--- backbone.py
+++ backbone.py
@@ -4,9 +4,9 @@ import multiprocessing as mproc
 import time
 
 start_time = time.time()
-window_size = 10
+window_size = 4
 
-w_pool = mproc.Pool(processes = 15, maxtasksperchild = 2)
+w_pool = mproc.Pool(processes = 10, maxtasksperchild = 1)
 
 img_dict = load_imgs.get_dictionary("./Chameleon_segmented/")
 print "img_dict ready!"
@@ -17,8 +17,12 @@ for (k, img) in img_dict.iteritems() :
                                     (img, window_size))
 print "Jobs sent!"
 words_dict = {}
+num_tasks = len(results)
 for (k, res) in results.iteritems() :
     words_dict[k] = results[k].get()
+    num_tasks -= 1
+    print k, "task collected"
+    print num_tasks, "tasks left"
 
 print "Hello words!"
 print "exec time =", (time.time() - start_time)
