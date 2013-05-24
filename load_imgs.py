1c11b283b40db729e78a815bd558d728d6c95733
djole <dorde.grbic@gmail.com>
2013-05-24 10:44:20 +0200

    Add rescaling of input images

--- load_imgs.py
+++ load_imgs.py
@@ -11,7 +11,7 @@ def get_dictionary(path_to_dir):
         Returns the dictionary containing image filename as label and
         image as value (image type is cvmat)."""
    
-
+    SCALE_TO = 1000
     files = [ filename
              for filename in listdir(path_to_dir)
              if isfile(join(path_to_dir,filename)) ]
@@ -27,8 +27,11 @@ def get_dictionary(path_to_dir):
             
         # Image creation
         img_bgr = cv2.imread(flnm, cv2.CV_LOAD_IMAGE_UNCHANGED)
-        img = cv2.cvtColor(img_bgr, cv2.cv.CV_BGR2YCrCb)
-        
+        img_big = cv2.cvtColor(img_bgr, cv2.cv.CV_BGR2YCrCb)
+        big_dim = max(img_big.shape)
+        if big_dim > SCALE_TO: scale = SCALE_TO/float(big_dim)
+        else: scale = 1
+        img = cv2.resize(img_big, (0,0), fx=scale, fy=scale)
         #Add img to the idx_pic_dict dictionary
         if (img == None): ## Check for invalid input
             print "Could not open or find the image"
@@ -42,11 +45,8 @@ def get_dictionary(path_to_dir):
 if __name__ == "__main__":
     pics_dict = get_dictionary("./test_pics")
     
-    """ Should be 001, 006, 020, 101, 101a as keys
-        and: green face right cham., green face left cham., brown cham.,
-        two white chams.
-    """
     for k, v in pics_dict.iteritems():
+        print v.shape
         cv2.namedWindow(k,2)
         cv2.resizeWindow(k, 800, 600)
         cv2.imshow(k, v)
