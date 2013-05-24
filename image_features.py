1db43fb2f931c6c67ddc44ea1ecebb72ceee220b
djole <dorde.grbic@gmail.com>
2013-05-24 10:43:11 +0200

    Add unit tests. Add __len__ for histogram class

--- image_features.py
+++ image_features.py
@@ -4,8 +4,9 @@ Created on May 7, 2013
 @author: djordje
 '''
 import numpy as np
+import unittest
 
-class SpatialWordFeature:
+class SpatialWordFeature(object):
     
     def __init__(self, achrom_word, chrom_word):
         self._achrom_word = achrom_word
@@ -16,8 +17,7 @@ class SpatialWordFeature:
     def get_chrom_word(self):
         return self._chrom_word
 
-class SpatialWordHistogram:
-    _histogram = None
+class SpatialWordHistogram(object):
     def __init__(self, hist_size):
         self._histogram = np.zeros(hist_size)
     
@@ -27,6 +27,9 @@ class SpatialWordHistogram:
     
     def __getitem__(self, idx):
         return self._histogram[idx]/self._histogram.sum()
+    
+    def __len__(self):
+        return len(self._histogram)
         
 def make_codebook_histogram(words_set, codebook):
     
@@ -43,3 +46,9 @@ def make_codebook_histogram(words_set, codebook):
         code_histogram.increment(min_distance_code)
         
     return code_histogram
+
+class Tests(unittest.TestCase):
+    def test_spatial_hist(self):
+        self.assert_(len(SpatialWordHistogram(5)) == 5, "Hist size error")
+if __name__ == '__main__':
+    unittest.main()
