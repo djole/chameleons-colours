7dfcdcade44167f8ac2090a8d0104951115020e9
djole <dorde.grbic@gmail.com>
2013-06-05 16:42:39 +0200

    Add loading of parameters from the '\''parameters.txt'\'' file. Add '\''...making histograms'\''status prints and change some prints. Add histogram distance weight from parameters file. Add word writing to files.

--- backbone.py
+++ backbone.py
@@ -5,16 +5,17 @@ import multiprocessing as mproc
 import time
 import image_features
 import pickle
-import gc
 from hierarch_clustering import Complete_link_custering
 from distance import Histograms_distance
+import codeword_visuals as word_vis
+import parameters as P
 
 start_time = time.time()
-window_size = 10
+window_size = P.parameters['window_size']
 
-w_pool = mproc.Pool(processes = 13)
+w_pool = mproc.Pool(processes = P.parameters['num_parallel'])
 
-img_dict = load_imgs.get_dictionary("/home/djordje/code/chameleons/Chameleon_segmented/")
+img_dict = load_imgs.get_dictionary(P.parameters['input_direct'])
 print "img_dict ready!"
 
 results = {}
@@ -37,10 +38,12 @@ print "Hello words!"
 print num_of_words, "words in the bag"
 print "exec time =", (time.time() - start_time)
 (achrom_codebook, chrom_codebook) = \
-    codebook.make_achrom_chrom_codebooks(words_dict,
-                                         num_train_iter=100,
-                                         num_codes=50,
-                                         training_fact=0.1)
+    codebook.make_achrom_chrom_codebooks(
+                                    words_dict,
+                                    num_train_iter=P.parameters['train_iter'],
+                                    num_codes=P.parameters['num_codewords'],
+                                    training_fact=P.parameters['training_factor']
+                                    )
 
 histogram_dictionary = {}
 
@@ -51,23 +54,40 @@ for (k, v) in words_dict.iteritems():
         bag_achrom.append(w.get_achrom_word())
         bag_chrom.append(w.get_chrom_word())
     
+    print "making achrom histograms"
     achrom_hist = \
             image_features.make_codebook_histogram(bag_achrom,achrom_codebook)
+    print "making chrom histograms"
     chrom_hist = \
             image_features.make_codebook_histogram(bag_chrom, chrom_codebook)
             
     histogram_dictionary[k] = (achrom_hist, chrom_hist)
 
-distance = Histograms_distance()
+distance = Histograms_distance(chrom_weight=P.parameters['chrom_hist_weight'],
+                               achrom_weight=P.parameters['achrom_hist_weight'])
 clustering = Complete_link_custering(distance)
+print "start clustering"
 top_node = clustering.fit(histogram_dictionary)
 
+
+'''Saving the results'''
 pickle_file = open("./top_node_save", 'w')
 pickle.dump(top_node, pickle_file)
 newick_file = open("./newick_tree.tre", 'w')
 newick_file.write(top_node.get_newick_string())
-words_dict = None
-gc.collect()
+word_vis.save_codewords(achrom_codebook,
+                        word_vis.ACHROMATIC,
+                        (window_size, window_size),
+                        './'+P.parameters['achrom_codewords_name']+'.tiff')
+word_vis.save_codewords(chrom_codebook,
+                        word_vis.CHROMATIC,
+                        (window_size, window_size),
+                        './'+P.parameters['chrom_codewords_name']+'.tiff')
 
-# Init the codebooks
-print "here"
+'''Making histograms' visualisation'''
+for k,v in histogram_dictionary.iteritems():
+    word_vis.draw_1histogram(histogram_dictionary[k][0], k, 'achrom')
+    word_vis.draw_1histogram(histogram_dictionary[k][1], k, 'chrom')
+    
+
+print "the end"
