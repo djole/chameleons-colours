f417340ee007ad0687ee1914fbe4f0a1f190c0aa
djole <dorde.grbic@gmail.com>
2013-06-25 18:26:17 +0200

    - Add a possibility to have a predictable random initializationby defining the seed value. @Class:Codebook: - Add '_word_size' and '_num_words' fields. - Add method copy(self) to copy the self object. @Class:FSC_learning: - Make the progress print refresh in the same line.
    @Module
    -Don't train the channel that has 0.0 weight associated with it in the histogram distance measure.

--- codebook.py
+++ codebook.py
@@ -7,13 +7,21 @@ import numpy as np
 import unittest
 import codeword_visuals as word_vis
 import parameters as P
+from sys import stdout
 CODEBOOK_FSCL = 0
 
+'''Set predictable sequence of random numbers'''
+if P.parameters.has_key("rand_seed"):
+    seed = P.parameters["rand_seed"]
+    np.random.seed(seed)
+    
 
 class Codebook(object):
     
     def __init__(self, num_words, word_size):
         self._codes = []
+        self._word_size = word_size
+        self._num_words = num_words
         for _ in xrange(num_words):
             ar = np.random.random_sample((word_size,))
             self._codes.append(ar)
@@ -25,6 +33,14 @@ class Codebook(object):
         if not isinstance(value, np.ndarray):
             raise TypeError("value must be numpy.ndarray type")
         self._codes[key] = value
+        
+    def copy(self):
+        codebk_cp = Codebook(self._num_words, self._word_size)
+        codebk_cp._codes = []
+        for i in xrange(self._num_words):
+            ar = np.array(self._codes[i])
+            codebk_cp._codes.append(ar)
+        return codebk_cp
     
     def __len__(self):
         return len(self._codes)
@@ -44,9 +60,11 @@ class FSC_learning(object):
         codebook = Codebook(num_codes, word_size)
         print "words to train on =", word_count
         for t_iter in xrange(train_iterations):
-            print "training iter :", t_iter
+            print "\ntraining iter :", t_iter
             for w_iter in xrange(word_count):
-                if w_iter % 10000 == 0: print w_iter,"/",word_count
+                if w_iter % 10000 == 0:
+                    stdout.write('\r %d / %d    '% (w_iter,word_count))
+                    stdout.flush()
                 sample = training_set[w_iter]
                 
                 min_distance_code = 0
@@ -62,7 +80,7 @@ class FSC_learning(object):
                 word_hit_count[min_distance_code] += 1
                 win_code = codebook[min_distance_code]
                 win_code += learn_fact * (sample - win_code)
-
+                
         return codebook
 
 
@@ -82,38 +100,58 @@ def make_achrom_chrom_codebooks(words_dict,
             training_set_chrom.append(f.get_chrom_word())
             achrombag_per_ind.append(f.get_achrom_word())
             chrombag_per_ind.append(f.get_chrom_word())
-        word_vis.save_codewords(achrombag_per_ind,
+        
+        '''Visualization of words in form of a picture.
+            Can be turned off.'''
+        if P.parameters['img_verbose']:
+            word_vis.save_words(achrombag_per_ind,
                                 word_vis.ACHROMATIC,
                                 (win_size,win_size),
-                                './words/'+k+'_achrom.tiff')
-        word_vis.save_codewords(chrombag_per_ind,
+                                P.parameters['words_dict']+k+'_achrom.tiff',
+                                codewords=False)
+            word_vis.save_words(chrombag_per_ind,
                                 word_vis.CHROMATIC,
                                 (win_size,win_size),
-                                './words/'+k+'_chrom.tiff')
+                                P.parameters['words_dict']+k+'_chrom.tiff',
+                                codewords=False)
     
-    '''Experimetal part. Delete.'''
-    word_vis.save_codewords(training_set_achrom,
+    '''Visualization part'''
+    if P.parameters['img_verbose']:
+        word_vis.save_words(training_set_achrom,
                             word_vis.ACHROMATIC,
                             (win_size,win_size),
-                            './words/bag_achrom.tiff')
-    word_vis.save_codewords(training_set_chrom,
+                            P.parameters['words_dict']+'bag_achrom.tiff',
+                            codewords=False)
+        word_vis.save_words(training_set_chrom,
                             word_vis.CHROMATIC,
                             (win_size,win_size),
-                            './words/bag_chrom.tiff')
-    '''End of experimental part'''
+                            P.parameters['words_dict']+'bag_chrom.tiff',
+                            codewords=False)
     
-    print "Training achrom codebook"
-    achrom_codebook = FSC_learning.make_codebook(training_set_achrom, num_codes,
-                                                 num_train_iter, training_fact)
+    # If the weight is zero it's not useless to train codewords.
+    # Just initialize one at random
+    if P.parameters['achrom_hist_weight'] > 0.0:
+        print "Training achrom codebook"
+        achrom_codebook = FSC_learning.make_codebook(training_set_achrom,
+                                                      num_codes,
+                                                      num_train_iter,
+                                                      training_fact)
+    else:
+        achrom_codebook = Codebook(num_words=1,
+                                   word_size=len(training_set_achrom[0]))
     
-    print "Training chrom codebook"
-    chrom_codebook = FSC_learning.make_codebook(training_set_chrom, num_codes,
-                                                num_train_iter, training_fact)
+    if P.parameters['chrom_hist_weight'] > 0.0:
+        print "Training chrom codebook"
+        chrom_codebook = FSC_learning.make_codebook(training_set_chrom,
+                                                     num_codes,
+                                                     num_train_iter,
+                                                     training_fact)
+    else:
+        chrom_codebook = Codebook(num_words=1,
+                                  word_size=len(training_set_chrom[0]))
     
     return (achrom_codebook, chrom_codebook)
-    
-    
-    
+
 '''Unit tests class'''
 class FSCL_tests(unittest.TestCase):
     
@@ -129,5 +167,3 @@ class FSCL_tests(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-    
-    
